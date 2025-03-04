2080a247e46c7e1535eaf1e3dc1f5dfd0d852084
Vishwajeet Kumar <vishwa843113@gmail.com>
2025-03-04 18:17:31 +0530

    Update NumbersAreEqual.py

---
+++ NumbersAreEqual.py
@@ -0,0 +1,7 @@
+def threeNumberAreEqual(a,b,c):
+    if a == b == c:
+        print("All numbers are equal.")
+    else:
+        print("All numbers are not equal.")
+a,b,c=8,8,8
+threeNumberAreEqual(a,b,c)
